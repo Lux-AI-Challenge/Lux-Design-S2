26852b91e7c5482e0f9646316b31dd8017c2d06c
James Camacho <camacho.james@gmail.com>
2022-07-10 19:05:39 -0600

    Reverted back to other timing as the cProfile wasn't so accurate.

--- luxai2022/map_generator/timing.py
+++ luxai2022/map_generator/timing.py
@@ -1,19 +1,9 @@
-from generator import *
-import cProfile
-import pstats
+import timeit
 
-num_runs = 100
-import time
-t = time.time()
+N = 100
 for map_type in ["cave", "craters", "island", "mountain"]:
-    exec(f"{map_type}(64, 64)") # for numba caching
-    with cProfile.Profile() as pr:
-        for i in range(num_runs):
-            exec(f"{map_type}(64, 64)")
-    sortby = pstats.SortKey.CUMULATIVE
-    ps = pstats.Stats(pr).sort_stats(sortby)
+    time = timeit.timeit(f"{map_type}(64, 64)", setup=f"from generator import {map_type}", number=N)
+    print(f"Took {time / N * 1000:.2f}ms for {map_type}.")
 
-    # You can get a lot more info out of this, but this suffices.
-    for fcn, profile in ps.get_stats_profile().func_profiles.items():
-        if fcn == map_type:
-            print(f"{map_type} took {profile.tottime / num_runs * 10000 : .2f} ms.")
+
+# If you want to use cProfile, you can look in git history.
