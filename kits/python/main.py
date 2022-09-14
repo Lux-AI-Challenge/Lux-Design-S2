0ec4ebe96f772019540f3a4b75a1059730519f4e
stone_tao <stonezt2019@gmail.com>
2022-09-14 03:10:29 -0700

    Update main.py

--- kits/python/main.py
+++ kits/python/main.py
@@ -15,14 +15,17 @@ if __name__ == "__main__":
             raise SystemExit(eof)
     step = 0
     player_id = 0
+    configurations = None
+    i = 0
     while True:
         inputs = read_input()
         obs = json.loads(inputs)
-        # with open("log", "w") as f:
-            # f.writelines(json.dumps(obs))
-        observation = Namespace(**dict(step=obs["step"], obs=json.dumps(obs["obs"]), remainingOverageTime=obs["remainingOverageTime"], player=obs["player"]))
-
+        
+        observation = Namespace(**dict(step=obs["step"], obs=json.dumps(obs["obs"]), remainingOverageTime=obs["remainingOverageTime"], player=obs["player"], info=obs["info"]))
+        if i == 0:
+            configurations = obs["info"]["env_cfg"]
+        i += 1
         # data = json.dumps(dict(obs=json.loads(observation.obs), step=observation.step, remainingOverageTime=observation.remainingOverageTime, player=observation.player, reward=observation.reward))
         # observation["updates"].append(inputs)
-        actions = agent_fn(observation, dict())
+        actions = agent_fn(observation, configurations)
         print(json.dumps(actions))
