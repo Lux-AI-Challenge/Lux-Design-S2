dcd3c2056fe379ecb0a1b0be28e4eedb50c7002d
themmj <themmj@users.noreply.github.com>
2022-09-15 22:48:33 +0200

    [cpp] Add nlohmann's json library

--- kits/cpp/src/main.cpp
+++ kits/cpp/src/main.cpp
@@ -1,1 +1,14 @@
-int main() { return 0; }
+#include <iostream>
+
+#include "lux/json.hpp"
+
+int main() {
+    while (true) {
+        json input;
+        std::cin >> input;
+
+        json output = json::parse(R"({"faction": "AlphaStrike", "bid": 10})");
+        std::cout << output << std::endl;
+    }
+    return 0;
+}
