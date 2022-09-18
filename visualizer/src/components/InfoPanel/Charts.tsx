895fc65f63ba2cbc7776a36fc1c6d1cb7ba9093b
stone_tao <stonezt2019@gmail.com>
2022-09-17 17:39:55 -0700

    charts

--- visualizer/src/components/InfoPanel/Charts.tsx
+++ visualizer/src/components/InfoPanel/Charts.tsx
@@ -1,132 +1,123 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+// import {
+//   Chart as ChartJS,
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend,
+//   ChartOptions,
+//   LineOptions,
+// } from 'chart.js';
+// ChartJS.register(
+//   CategoryScale,
+//   LinearScale,
+//   PointElement,
+//   LineElement,
+//   Title,
+//   Tooltip,
+//   Legend
+// );
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
+  CartesianGrid,
   Legend,
-  ChartOptions,
-  LineOptions,
-} from 'chart.js';
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
+  Line,
+  LineChart,
+  XAxis,
+  YAxis,
+  ResponsiveContainer,
   Tooltip,
-  Legend
-);
+  Label,
+} from 'recharts';
 import s from "./charts.module.scss"
-import {Line} from 'react-chartjs-2'
 import { useStore, useStoreKeys } from '@/store';
-export const Charts = () => {
-  // const labels = Utils.months({count: 7});
-  const labels = [0,1,2,3,4,5,6,7,8,9,10];
-  const defaultFontOptions = {
-    color: 'white',
-    font: {
-      size: 14,
-    }
-  }
-  const defaultOptions: ChartOptions<'line'> = {
-    responsive: true,
-    plugins: {
-      legend: {
-        position: 'top' as const,
-        labels: {
-          color: 'white',
-          font: {
-            size: 14
-          }
-        }
-      },
-    },
-    scales: {
-      x: {
-        title: {
-          ...defaultFontOptions,
-          display: true,
-          text: 'Steps',
-          font: {
-            size: 14,
-            weight: "600"
-          }
-        },
-        grid: {
-          color: 'rgba(255,255,255,0.2)',
-          borderColor: 'rgba(255,255,255,0.5)',
-        },
-        ticks: {
-          ...defaultFontOptions,
-        }
-      },
-      y: {
-        title: {
-          ...defaultFontOptions,
-          display: true,
-          text: 'Count',
-          font: {
-            size: 14,
-            weight: "600"
-          }
-        },
-        grid: {
-          color: 'rgba(255,255,255,0.2)',
-          borderColor: 'rgba(255,255,255,0.5)',
-        },
-        beginAtZero: true,
-        ticks: {
-          ...defaultFontOptions,
-        }
-      }
-    }
-  };
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: 'P0 Light Robots',
-      data: [0, 1,10, 20],
-      fill: false,
-      borderColor: 'rgb(175, 192, 192)',
-      tension: 0.1
-    }, {
-      label: 'P0 Heavy Robots',
-      data: [65, 59, 80, 81, 56, 55, 40],
-      fill: false,
-      borderColor: 'rgb(75, 192, 192)',
-      tension: 0.1
-    },{
-      label: 'P1 Light Robots',
-      data: [1, 5, 21, 30, 55, 40],
-      fill: false,
-      borderColor: 'rgb(192, 75, 192)',
-      tension: 0.1
-    },{
-      label: 'P1 Heavy Robots',
-      data: [20, 30, 40],
-      fill: false,
-      borderColor: 'rgb(192, 175, 192)',
-      tension: 0.1
-    }]
-  };
-  const replay = useStore((state) => state.replay)!; // game map should only get rendered when replay is non-null
-  const { turn, speed, updateGameInfo, tileWidth } = useStoreKeys(
+export const Charts = React.memo(() => {
+  // const replay = useStore((state) => state.replay)!; // game map should only get rendered when replay is non-null
+  const { turn, replayStats } = useStoreKeys(
     "turn",
-    "speed",
-    "updateGameInfo",
-    "tileWidth"
+
+    "replayStats"
   );
-  const frame = replay.observations[turn];
+  const [labels, setLabels] = useState<Array<number>>([]);
+  const [data, setData]= useState<any>([]);
+  console.log("render")
+  useEffect(() => {
+    const d = replayStats.frameStats.slice(0, turn + 1).map((v, i) => {
+      return {name: `Turn ${i}`, p0light:v["player_0"].units.light, p1light:v["player_1"].units.light};
+    });
+    const labels = [];
+    for (let i = 0; i < data.length; i++) {
+      labels.push(i);
+    }
+    setLabels(labels);
+    setData(d);
+  }, [turn]);
+  const xlabel = "Turn"
+  const ylabel = "Count";
+  const TEAM_A_COLOR_STR = "#007D51";
+  const TEAM_B_COLOR_STR = "#0082FB";
   return (
     <>
-      <div>
+      {/* <div>
         <h2>Robots</h2>
-        <div className={s.chartWrapper}><Line options={{...defaultOptions}} data={data} /></div>
-      </div>
+        <div className={s.chartWrapper}><LineMemo options={{...defaultOptions}} data={data} /></div>
+      </div> */}
+      <div className="Graph">
+      <ResponsiveContainer width={'100%'} height={240}>
+        <LineChart
+          // width={200}
+          onClick={() => {}}
+          // height={150}
+          data={data}
+          margin={{ top: 15, right: 20, left: 10, bottom: 25 }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="name">
+            <Label
+              value={xlabel}
+              offset={-15}
+              position="insideBottom"
+              fill="#f9efe2"
+            />
+          </XAxis>
+          <YAxis
+            label={{
+              value: ylabel,
+              angle: -90,
+              position: 'insideLeft',
+              fill: '#f9efe2',
+              color: 'f9efe2',
+            }}
+          ></YAxis>
+          <Tooltip labelStyle={{ color: '#323D34' }} />
+          {/* <Legend
+            verticalAlign="top"
+            height={36}
+            formatter={renderColorfulLegendText}
+          /> */}
+          <Line
+            type="monotone"
+            dataKey="p0light"
+            name="Player 0"
+            dot={false}
+            strokeWidth={2}
+            isAnimationActive={false}
+            stroke={TEAM_A_COLOR_STR}
+          />
+          <Line
+            type="monotone"
+            dataKey="p1light"
+            name="Player 1"
+            dot={false}
+            strokeWidth={2}
+            isAnimationActive={false}
+            stroke={TEAM_B_COLOR_STR}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
     </>
   )
-};
+});
