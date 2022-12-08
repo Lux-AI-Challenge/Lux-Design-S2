cf2184a59afeeb2fc5be58ffd152799b5ab57f8f
Liqimai <liqimai@qq.com>
2022-12-09 01:29:08 +0800

    Fix a bug in compute_water_info
    Fix a bug in compute_water_info
    
    The bug leads to unexpected connection between lichen strains.
    For example, on a 4x1 map. There is a lichen strain A. The third tile of lichen has been digged out by enemy robots. However, with the bug, the two part of lichen strains are still considered as connected, and will be watered together.
    
    ```
    |A|
    |A|
    | |
    |A|
    ```

--- luxai2022/factory.py
+++ luxai2022/factory.py
@@ -36,36 +36,35 @@ def compute_water_info(init: np.ndarray, MIN_LICHEN_TO_SPREAD: int, lichen: np.n
         if forbidden[pos[0], pos[1]]:
             continue
         pos_lichen = lichen[pos[0], pos[1]]
+        pos_strain = lichen_strains[pos[0], pos[1]]
         # check for surrounding tiles with lichen and no incompatible lichen strains, grow on those
         can_grow = True
         for move_delta in move_deltas[1:]:
             check_pos = pos + move_delta
             # check surrounding tiles on the map
             if check_pos[0] < 0 or check_pos[1] < 0 or check_pos[0] >= H or check_pos[1] >= W: continue
+
+            # it the check_pos belongs to another strain, we can't grow in current pos.
             adj_strain = lichen_strains[check_pos[0], check_pos[1]]
+            if adj_strain != -1 and adj_strain != strain_id:
+                can_grow = False
+
+            # if seen, skip
             if (check_pos[0], check_pos[1]) in seen:
-                # even if it is seen, we still need to check its strain, to decide weather current pos is growable.
-                # but there is no need to add it to the frontier again.
-                if adj_strain != -1 and adj_strain != strain_id:
-                    can_grow = False
                 continue
-            if adj_strain == -1:
-                if pos_lichen >= MIN_LICHEN_TO_SPREAD:
-                    # adjacent tile is empty and isn't a resource and the current tile has enough lichen to spread
-                    seen.add(tuple(check_pos))
-                    frontier.append(check_pos)
-            elif adj_strain != strain_id:
-                    # adjacent tile is not empty and is not a strain this factory owns.
-                    can_grow = False
-                    seen.add(tuple(check_pos))
-            else:
-                # adjacent tile has our own strain, we can grow here too
+
+            # we add it to the frontier only in two cases:
+            #  1. it is an empty tile, and current pos has enough lichen to expand.
+            #  2. both current tile and check_pos are of our strain.
+            if (adj_strain == -1 and pos_lichen >= MIN_LICHEN_TO_SPREAD) \
+                or (adj_strain == strain_id and pos_strain == strain_id):
                 seen.add(tuple(check_pos))
                 frontier.append(check_pos)
 
         if can_grow or (lichen_strains[pos[0], pos[1]] == strain_id):
             grow_lichen_positions.add((pos[0], pos[1]))
     return grow_lichen_positions
+
 
 class Factory:
     def __init__(self, team: Team, unit_id: str, num_id: int) -> None:
