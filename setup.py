5378b1eccb550914f1b54cee0e71e61dc862d417
stone_tao <stonezt2019@gmail.com>
2022-12-08 22:49:23 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name="luxai2022",
-    version="1.1.2",
+    version="1.1.3",
     author="Lux AI Challenge",
     description="The Lux AI Challenge Season 2",
     license="MIT",
