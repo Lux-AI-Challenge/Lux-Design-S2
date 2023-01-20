fe4c83a076e0a9dd75247dca4ee7977e09ad7ee4
Jasper van Merle <jaspervmerle@gmail.com>
2023-01-20 14:21:50 +0100

    Remove hash from production files

--- lux-eye-s2/vite.config.ts
+++ lux-eye-s2/vite.config.ts
@@ -4,4 +4,13 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
 });
