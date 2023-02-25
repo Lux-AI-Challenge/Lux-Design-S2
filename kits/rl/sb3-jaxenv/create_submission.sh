5fddf53409e5c6c93ef2483886c860dc07adda4f
StoneT2000 <stonezt2019@gmail.com>
2023-02-25 14:11:12 -0800

    working submission using jax

--- kits/rl/sb3-jaxenv/create_submission.sh
+++ kits/rl/sb3-jaxenv/create_submission.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
+mkdir packages
+JUX_PATH=$(pip show juxai_s2 | grep Location | awk '{print $2}')/jux
+echo "Copying Jux" $JUX_PATH
+cp -r $JUX_PATH ./packages
 
+LUX_PATH=$(pip show luxai_s2 | grep Location | awk '{print $2}')/luxai_s2
+echo "Copying Lux" $LUX_PATH
+cp -r $LUX_PATH ./packages
+cp -r packages/* .
+
+rm submission.tar.gz
+tar -cvzf submission.tar.gz * > /dev/null
+
+rm -rf jux
+rm -rf luxai_s2
